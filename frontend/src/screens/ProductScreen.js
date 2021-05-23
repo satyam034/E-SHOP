980025e151bc0ad75c3a9bfc763f3064e35e7fff
satyam034 <satyamb954@gmail.com>
2021-05-23 19:56:14 +0530

    'nodemon_and_concurrently_installed'

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
@@ -14,7 +14,7 @@ const ProductScreen = ({ match }) => {
         }
 
         fetchProducts();
-    },[]);
+    },[match]);
 
     return (
         <>
