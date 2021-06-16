3966ba811da4e29f81f2921ed89d6b77ec3ed612
satyam034 <satyamb954@gmail.com>
2021-06-16 08:09:28 +0530

    'user_registration_and_password_encryption'

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,6 +23,39 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc register user
+// @route POST /api/users
+//@access private
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+    const userExist = await User.findOne({ email })
+
+    if(userExist){
+        res.status(400)
+        throw new Error("User already exist")
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else{
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
+   
+})
+
 // @desc get userprofile
 // @route GET /api/users/profile
 //@access private
@@ -44,5 +77,6 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 export {
    authUser,
-   getUserProfile
+   getUserProfile,
+   registerUser
 }
