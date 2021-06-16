244966c43d1cb04d16ed2b99eb770b5d8b30511d
satyam034 <satyamb954@gmail.com>
2021-06-16 07:37:51 +0530

    'custom_authentication_middleware'

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,6 +23,26 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc get userprofile
+// @route GET /api/users/profile
+//@access private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else{
+        res.status(404)
+        throw new Error
+    }
+})
+
 export {
-   authUser
+   authUser,
+   getUserProfile
 }
